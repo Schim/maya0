1f31228605dc346e01114ed2b978a7702503f2e6
Schim <schim@gmx.com>
2012-07-02 11:05:03 +0200

    cc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maya0::Application.config.secret_token = 'e68c8f16c54002c4cfcf65ecec9e592ecb7d8ff6e182380ed3ad4c47f527e43836a280c9822f318bf99044fccd4d725c263a6c5a3b31610edc7fce33dc713ec8'
